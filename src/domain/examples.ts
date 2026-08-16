ab86ce9559f49b74ea830391b7182577a39629f1
Laggy <71883024+ALaggyDev@users.noreply.github.com>
2026-08-17 07:18:50 +0800

    Add Doughnut SMP examples

--- src/domain/examples.ts
+++ src/domain/examples.ts
@@ -7,15 +7,27 @@ export const exampleProjects = [
   {
     id: 'dark-cave',
     name: 'Dark Cave',
-    description: '',
+    description: 'A dark cave, duh.\n\nLocation: (-135, -27, -578)',
     bundleSrc: '/examples/dark-cave.wcf',
   },
   {
     id: 'bright-cavern',
     name: 'Bright Cavern',
-    description: '',
+    description: 'A cavern with fullbright.\n\nLocation: (1564, -45, -1230)',
     bundleSrc: '/examples/bright-cavern.wcf',
   },
+  {
+    id: 'doughnut-smp-easy-',
+    name: 'Doughnut SMP (Easy)',
+    description: 'DumbKid67 shared this screenshot of his base on Doughnut SMP🔥! Can you find the coordinates?\n\nLocation: (197325, -50, -219011)',
+    bundleSrc: '/examples/doughnut-smp-easy-.wcf',
+  },
+  {
+    id: 'doughnut-smp-hard-',
+    name: 'Doughnut SMP (Hard)',
+    description: 'Same as the easy version, but now the screenshot is more close up.\n\nHint: You may want to start with the center of the image to fit the camera and work your way outwards.\nLocation: (197325, -50, -219011)',
+    bundleSrc: '/examples/doughnut-smp-hard-.wcf',
+  }
 ] as const
 
 export type ExampleProjectId = (typeof exampleProjects)[number]['id']
